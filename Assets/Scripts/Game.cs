46fa50e1798610824c51471700dbfd8719789fd2
Dragonfactor <dorian.scrimshaw@student.sl.on.ca>
2022-11-13 16:21:38 -0500

    Added Robot Spawning

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+    private static Game singleton;
+    [SerializeField]
+    RobotSpawn[] spawns;
+    public int enemiesLeft;
+    void Start()
+    {
+        singleton = this;
+        SpawnRobots();
+    }
+    // 2
+    private void SpawnRobots()
+    {
+        foreach (RobotSpawn spawn in spawns)
+        {
+            spawn.SpawnRobot();
+            enemiesLeft++;
+        }
+    }
+}
